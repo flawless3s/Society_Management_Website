3dccf50b224c9c91680ddff78187f78b93c369ed
flawless3s <marcdaniel2506@gmail.com>
2024-07-08 18:24:25 +0530

    timeout error

--- fetching_image.py
+++ fetching_image.py
@@ -1,4 +1,9 @@
 import requests
 def fetch_image_from_google_drive(google_drive_url):
-    response = requests.get(google_drive_url)
-    return response.content
+    try:
+        response = requests.get(google_drive_url, timeout=30)
+        return response.content  # Increase timeout to 10 seconds or more
+    # Process the response
+    except requests.exceptions.Timeout:
+        print("Request timed out. Check your connection.")
+    
